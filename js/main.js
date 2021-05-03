23ba027e74e95aa4746e356282a61da89c849a85
YoshihiroIshizka <mdmdmdmdmd57@gmail.com>
2021-05-03 19:48:50 +0900

    四則演算

--- js/main.js
+++ js/main.js
@@ -1,13 +1,7 @@
 'use strict';
 
-console.log('ようこそ世界へ');
-console.log('ようこそ世界へ');
-console.log('ようこそ世界へ');
-console.log('ようこそ世界へ');
-console.log('ようこそ世界へ');
-
-console.log("it's me!"); 
-console.log('it\'s me!');
-console.log('よう\nこそ\t世界へ');
-console.log('ようこそ'+'世界へ');
-
+console.log(4+3);
+console.log(4-3);
+console.log(4%3);
+console.log(4/3);
+console.log(4*3);
