87601b5a8e4f071456351ec04ff7e11ab0df3369
YoshihiroIshizka <mdmdmdmdmd57@gmail.com>
2021-05-03 19:45:46 +0900

    ・''と””,\や\n,\t,+の使い方など

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 #JSPraBasic
 JSの基礎~文法中心の学習記録です。
+
+・'use strictについて
+エラーチェックのために必ず記載
+
+・他ファイルからの実行
+※scriptは閉じタグを忘れない
+<!-- ex)<script src="js/main.js"></script> -->
+
+・''と””,\や\n,\t,+の使い方など
+
+●VScode関連
+
+・複製
+shift+option+↓
+
+・参照
+commandとクリック
---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+console.log('ようこそ世界へ');
+console.log('ようこそ世界へ');
+console.log('ようこそ世界へ');
+console.log('ようこそ世界へ');
+console.log('ようこそ世界へ');
+
+console.log("it's me!"); 
+console.log('it\'s me!');
+console.log('よう\nこそ\t世界へ');
+console.log('ようこそ'+'世界へ');
+
