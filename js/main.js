79004481958a89e264768496cdf94da8f4b16a66
YoshihiroIshizka <mdmdmdmdmd57@gmail.com>
2021-05-03 22:39:27 +0900

    スコープについて

--- js/main.js
+++ js/main.js
@@ -1,32 +1,13 @@
 'use strict';
 
-// function sum(a,b,c){
-//     // console.log(a + b + c);
-//     return a + b+ c ;
-// }
+const x=5;
 
-//定数関数に書き換えると
+function scp(){
+    const x =1;
+    console.log(x);
+}
 
-// const sum =function (a,b,c){
-//     // console.log(a + b + c);
-//     return a + b+ c ;
-// };
+scp();//1
+//下記はscpないでしか宣言していない場合エラー
+console.log(x)//5
 
-//アロー関数
-// const sum =(a,b,c)=>{
-//      return a + b+ c ;
-// };
-
-//さらに短く
-const sum =(a,b,c)=>a + b+ c ;
-
-const total =sum(2,3,4) + sum(9,4,4);
-console.log(total);
-
-// const double = function(a){
-//     return a*2;
-// };
-//書き換えると
-const double = a => a*2;
-
-console.log(double(12));
