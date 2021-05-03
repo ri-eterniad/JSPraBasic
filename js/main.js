62373937c32aa7d27249568e31fe51efa999b84a
YoshihiroIshizka <mdmdmdmdmd57@gmail.com>
2021-05-03 20:56:35 +0900

    条件分岐基礎

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 'use strict';
 
-//false 0,null,undifined,'',false
-//true それ以外
-console.log(Boolean(0));
-console.log(Boolean('hello'));
+const score = 69;
+
+if(score >70){
+    console.log('良い点数です')
+}else{
+    console.log('もう少し頑張りましょう')
+}
+
+if(score >85){
+    console.log('とても良い点数です')
+}else if(score >70){
+    console.log('良い点数です')
+}else{
+    console.log('もう少し頑張りましょう')
+}
